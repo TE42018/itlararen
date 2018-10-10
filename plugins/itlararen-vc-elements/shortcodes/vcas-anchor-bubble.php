68460a0b0d652c21ee520ef81b055079733299f1
Alex <alexander.emland@elev.ga.ntig.se>
2018-10-10 13:29:59 +0200

    bubble done

--- plugins/itlararen-vc-elements/shortcodes/vcas-anchor-bubble.php
+++ plugins/itlararen-vc-elements/shortcodes/vcas-anchor-bubble.php
@@ -1,5 +1,4 @@
 <?php
-
 function vcas_component_anchor_bubble() {
 vc_map(
     array(
@@ -10,7 +9,7 @@ vc_map(
             array(
                 'type' => 'attach_image',
                 'holder' => 'div',
-                'class' => 'anchor-bubble',
+                'class' => '',
                 'heading' => __( 'Image' ),
                 'param_name' => 'bubbleimg',
                 'value' => __( '' ),
@@ -29,8 +28,6 @@ vc_map(
         )
     );
 }
-
-
 add_action( 'vc_before_init', 'vcas_component_anchor_bubble' );
 
 function vcas_anchor_bubble_function( $atts, $content ) {
@@ -47,14 +44,16 @@ function vcas_anchor_bubble_function( $atts, $content ) {
 
 	if(!empty($atts['bubbleimg'])) {
         $image_id = $atts['bubbleimg'];
-        $image_url = wp_get_attachment_url($image_id, 'small');
+        $image_url = wp_get_attachment_url($image_id, 'large');
         $link = $atts['bubblelink'];
         
 		ob_start();
 		?>
-        <a href="">
-        <img src="<?php echo $image_url ?>" alt="Bubble image" /> 
-        </a>
+        <div >
+            <a class="anchor-bubble" href="<?php echo $link ?>">
+            <img src="<?php echo $image_url ?>" alt="Bubble image" /> 
+            </a>
+        </div>
         
 		<?php
 		$html .= ob_get_clean();
@@ -65,5 +64,12 @@ function vcas_anchor_bubble_function( $atts, $content ) {
 	return $html;
 }
 add_shortcode( 'vcas_anchor_bubble', 'vcas_anchor_bubble_function' );
+
+function vc_enqueue_style(){
+    wp_register_style( 'itlararen-vc-styles',  plugin_dir_url( __FILE__ ) . 'assets/style.css' );
+    wp_enqueue_style( 'itlararen-vc-styles' );
+}
+add_action( 'admin_enqueue_scripts', 'vc_enqueue_style' );
+
 
 ?>
